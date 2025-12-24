be03f10c540fed154f7766fd6fa0a940b1ab44d0
Pujitha Reddy Kethireddy <pujithareddy1357@gmail.com>
2025-12-24 08:54:14 +0000

    verilog

--- Verilog_Language/Basics/08_7458_chip.v
+++
@@ -1,9 +0,0 @@
-module top_module ( 
-    input p1a, p1b, p1c, p1d, p1e, p1f,
-    output p1y,
-    input p2a, p2b, p2c, p2d,
-    output p2y );
-    wire x1, y1,x2, y2;
-    assign {x1, y1,x2, y2} = {p1a & p1b & p1c, p1d & p1e & p1f, p2a & p2b, p2c & p2d};
-    assign {p1y, p2y} = {x1 | y1, x2 | y2}; 
-endmodule
