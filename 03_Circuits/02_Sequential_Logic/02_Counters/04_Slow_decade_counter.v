88cc9c43fb25e975be2dcc0330c2543e30b78284
Pujitha Reddy Kethireddy <pujithareddy1357@gmail.com>
2026-03-06 13:47:38 +0000

    latches and flipflops

---
+++ 03_Circuits/02_Sequential_Logic/02_Counters/04_Slow_decade_counter.v
@@ -0,0 +1,12 @@
+module top_module (
+    input clk,
+    input slowena,
+    input reset,
+    output [3:0] q);
+    always@(posedge clk)begin
+        if(reset)
+            q <= 4'd0;
+        else
+            q <= (q == 4'd9 & slowena)? 4'd0: (slowena ? q + 4'd1 : q);        
+    end
+endmodule
